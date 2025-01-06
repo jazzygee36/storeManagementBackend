ace90007d00dd659a18fa68241faebe9a97c2543
Samson Mesioye <jazzygee36gmail.com>
2025-01-06 14:05:28 +0100

    working

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
 const server = express();
+let cachedApp: any;
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  // Enable CORS
-  app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // If you need to allow cookies
-  });
-  await app.listen(process.env.PORT ?? 4000);
+export default async function handler(
+  req: express.Request,
+  res: express.Response,
+) {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    app.enableCors({
+      origin: 'https://your-frontend.vercel.app', // Update for production frontend URL
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+    await app.init();
+    cachedApp = server;
+  }
+  cachedApp(req, res);
 }
-bootstrap();
-
-export default server;
